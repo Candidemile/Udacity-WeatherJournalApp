42051f417b4da50500530b203bc92cf21eb6d59c
Candid_emile <doctor_net@mail.ru>
2020-06-18 18:28:08 +0300

    FEAT: Set up fetch for Open Weather Map API

--- commentsOnlyJS/app.js
+++ commentsOnlyJS/app.js
@@ -1,16 +1,23 @@
 // Personal API Key for OpenWeatherMap API
-const apikey = ;
-const url = "api.openweathermap.org/data/2.5/weather?";
-
+const apikey = 'f6a641105amsh3901a1f0f0ca125p1afce6jsnb6d0dd468558';
+const url = 'https://community-open-weather-map.p.rapidapi.com/weather?units=metric&';
+const reqBody = {
+    method: 'GET',
+    headers: {
+        'x-rapidapi-key': apikey
+    }
+};
 // Event listener to add function to existing HTML DOM element
-
+// document.getElementById('generate').addEventListener('click', getOpenWeatherData);
 /* Function called by event listener */
 
 /* Function to GET Web API Data*/
 const getOpenWeatherData = async () => {
-    
-}
+    const response = await fetch(url + 'q=192284', reqBody);
+    return response.json();
+};
+
 /* Function to POST data */
 
-
 /* Function to GET Project Data */
+getOpenWeatherData().then((data) => console.log(data));
