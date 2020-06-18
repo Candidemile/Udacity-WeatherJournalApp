1cdf28b85b4aa1e28d90dffc972524ead722ba17
Candid_emile <doctor_net@mail.ru>
2020-06-18 16:43:50 +0300

    FEAT: Set up GET and POST routes on the server

--- commentsOnlyJS/app.js
+++ commentsOnlyJS/app.js
@@ -1,11 +1,15 @@
 // Personal API Key for OpenWeatherMap API
+const apikey = ;
+const url = "api.openweathermap.org/data/2.5/weather?";
 
 // Event listener to add function to existing HTML DOM element
 
 /* Function called by event listener */
 
 /* Function to GET Web API Data*/
-
+const getOpenWeatherData = async () => {
+    
+}
 /* Function to POST data */
 
 
--- server.js
+++ server.js
@@ -3,17 +3,17 @@ projectData = {};
 
 // Require Express to run server and routes
 const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
 // Start up an instance of app
 const app = express();
+
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
+const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
-// Cors for cross origin allowance
-app.use(cors);
+// // Cors for cross origin allowance
+// const cors = require('cors');
+// app.use(cors);
 // Initialize the main project folder
 app.use(express.static('website'));
 
@@ -21,4 +21,20 @@ app.use(express.static('website'));
 const listening = () => {
     console.log('The server is running..');
 };
-app.listen(listening);
+app.listen(3000, listening);
+
+/* GET routes */
+app.get('/get', function(req, res) {
+    res.send(`Here is project data\n: ${Object.entries(projectData)}\n`);
+    console.log('GET request is being processed. Sending Project data.');
+});
+
+/* POST routes */
+app.post('/post', (req, res) => {
+    console.log('POST request is being processed. Adding new data to projectData.');
+    projectData.temperature = req.body.temperature;
+    projectData.date = req.body.date;
+    projectData.user_response = req.body.user_response;
+
+    console.log(`${Object.entries(projectData)}`);
+});
